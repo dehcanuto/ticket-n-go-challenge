7463641476bbb769da4e634a552ceccba28fa968
André Canuto <andrecanuto@MacBook-Pro-de-Andre.local>
2024-03-27 12:02:16 -0300

    feat: sidebar done

--- react/src/components/organisms/index.tsx
+++ react/src/components/organisms/index.tsx
@@ -1,4 +1,5 @@
 export { default as Header } from './Header'
 export { default as ProductsFlow } from './ProductsFlow'
 export { default as ProductSingle } from './ProductSingle'
-export { default as Cart } from '../organisms/Cart'
+export { default as Cart } from './Cart'
+export { default as MenuMobile } from './MenuMobile'
