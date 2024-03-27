aa4dbdfb3d14475d33d88e966fa5a97a3347ac4c
André Canuto <andrecanuto@MacBook-Pro-de-Andre.local>
2024-03-27 16:36:20 -0300

    feat: header done

---
+++ vue-nuxt/components/atoms/ActionLinks.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const emit = defineEmits(['click']);
+
+defineProps({
+    icon: {
+        type: String,
+        required: true
+    },
+    notified: Boolean,
+});
+</script>
+
+<template>
+    <button type="button" @click="() => emit('click')" class="flex items-center text-2xl hover:text-blue-400">
+        <Icon :name="icon" color="white" class="flex text-2xl" />
+        <span v-if="notified" class="flex absolute -mt-5 ml-4">
+            <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
+            <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500" />
+        </span>
+    </button>
+</template>
